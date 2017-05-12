316699316f3ec8e39d26492dfd75a752d2b67b02
杨虎 <yanghu@xiaoshouyi.com>
2017-05-12 11:46:47 +0800

    Default value and method of onChange

--- index.ios.js
+++ index.ios.js
@@ -26,9 +26,26 @@ let options = {};
 
 
 export default class TcombFormNativeDemo extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      value: {
+        name: 'hu',
+        surname: 'yang'
+      }
+    };
+  }
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+  onChange(value) {
+    console.log('=========');
+    console.log(value);
+    this.setState({value});
+  }
   onPress() {
     let value = this.refs.form.getValue();
-    if(value) {
+    if (value) {
       console.log('------------');
       console.log(value);
     }
@@ -37,8 +54,8 @@ export default class TcombFormNativeDemo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <RealForm ref='form' type={Person} options = {options} />
-        <TouchableHighlight style={styles.button} onPress={()=>this.onPress()} underlayColor='#99d9f4'>
+        <RealForm ref='form' type={Person} options={options} value={this.state.value} onChange={(value)=>this.onChange(value)}/>
+        <TouchableHighlight style={styles.button} onPress={() => this.onPress()} underlayColor='#99d9f4'>
           <Text style={styles.buttonText}>Save</Text>
         </TouchableHighlight>
       </View>
@@ -49,8 +66,8 @@ export default class TcombFormNativeDemo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    top:64,
-    marginHorizontal:15,
+    top: 64,
+    marginHorizontal: 15,
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
